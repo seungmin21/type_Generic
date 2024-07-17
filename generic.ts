a486b098d25fe3d5aecbb886699c56afc1713df1
ohseungmin <ohmin2886@gmail.com>
2024-07-17 17:06:21 +0900

    주석 부연 설명

--- generic.ts
+++ generic.ts
@@ -1,3 +1,11 @@
+
+/**
+ * arr getFirstElement 함수의 매개변수
+ * T는 타입의 매개변수
+ * T | undefined는 배열 속의 타입이
+ * 나오거나 배열 속에 인덱스가 없을 경우
+ * undefined로 나온다.
+ */
 function getFirstElement<T>(arr: T[]): T | undefined {
   return arr[0];
 }
